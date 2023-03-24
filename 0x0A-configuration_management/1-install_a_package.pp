ecef09283eb35a87fc47627f257ba911a9c03750
Ayo Adekunle <ay4eva2006@gmail.com>
2023-03-24 13:15:41 -0700

    fix error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
-ckage { 'python3-pip':
-  ensure => installed,
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
-}
-
