3e8223db8fcca1a0e88d76398f1a88a5bcbcd48e
Ayo Adekunle <ay4eva2006@gmail.com>
2023-03-26 12:35:23 -0700

    quick fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,16 @@
-hanges SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+# Client configuration file (w/ Puppet)
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
